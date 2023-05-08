95161d93daa4736b8e4ef62967536fdf3d0d3ac4
Jason-Wall <jason.waldick@gmail.com>
2023-05-07 19:48:05 -0600

    using table with db data

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import axios from 'axios';
-//MUI
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-//Custom Components
-import PSDChart from './Components/PSDChart';
-import Nav from './Components/Nav';
-import DataTable from './Components/DataTable';
-//Helper Functions
-import { rechartDataFormat, sampleSerials } from './Helpers/dataFormat';
-import { USCDistribution } from './Helpers/classification';
+import React, { Fragment } from 'react';
 
+//Custom Components
+import PSDDashboard from './Views/PSDDashboard';
+import Nav from './Components/Nav';
 
 //MAIN APPLICATION
 export default function App() {
   // State management and functions:
-  const [rechartData, setRechartData] = useState([]);
-  const [serials, setSerials] = useState([]);
-  const [classifications, setClassifications] = useState([]);
-
-  useEffect(() => {
-    //Get all PSD data from server:
-    axios.get('https://soilsserver-production.up.railway.app/psd/')
-      .then((res) => {
-        // console.log('axios.get All psd data', res.data);
-        const dbDump = res.data;
-        setRechartData(rechartDataFormat(dbDump));
-        setSerials(sampleSerials(dbDump));
-        let classifyArr = [];
-        classifyArr = dbDump.map((sample) => USCDistribution(sample.results));
-        setClassifications(classifyArr);
-      });
-
-  }, []);
-
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
 
   return (
     <Fragment>
       <Nav />
-      <PSDChart chartData={rechartData} serials={serials} classifications={classifications} />
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={4}>
-            <DataTable />
-          </Grid>
-          <Grid item xs={6} md={8}>
-            <DataTable />
-          </Grid>
-
-
-        </Grid>
-      </Box>
-
-
+      <PSDDashboard />
     </Fragment>
   );
 }
---
+++ src/Views/PSDDashboard.jsx
@@ -0,0 +1,68 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+//MUI
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Item from '../Components/Item';
+//Custom Components
+import PSDChart from '../Components/PSDChart';
+import Table from '../Components/Table';
+//Helper Functions
+import { rechartDataFormat, sampleSerials } from '../Helpers/dataFormat';
+import { USCDistribution } from '../Helpers/classification';
+
+//MAIN APPLICATION
+export default function App() {
+  // State management and functions:
+  const [rechartData, setRechartData] = useState([]);
+  const [serials, setSerials] = useState([]);
+  const [classifications, setClassifications] = useState([]);
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    //Get all PSD data from server:
+    axios.get('https://soilsserver-production.up.railway.app/psd/').then((res) => {
+      console.log('axios.get All psd data', res.data);
+      const dbDump = res.data;
+      setRechartData(rechartDataFormat(dbDump));
+      setSerials(sampleSerials(dbDump));
+
+      let tableArr = [];
+      tableArr = dbDump.map((sample) => {
+        sample.visible = true;
+        return sample;
+      });
+      setTableData(tableArr);
+
+      let classifyArr = [];
+      classifyArr = dbDump.map((sample) => USCDistribution(sample.results));
+      setClassifications(classifyArr);
+    });
+  }, []);
+
+  const sampleListCol = [
+    { field: 'sample_serial', headerName: 'Sample Serial', width: 150 },
+    { field: 'serial', headerName: 'Test Serial', width: 150 },
+    { field: 'test_date', headerName: 'Test Date', width: 150 },
+  ];
+
+  console.log('tableData', tableData);
+
+  return (
+    <Fragment>
+      <Grid>
+        <PSDChart chartData={rechartData} serials={serials} classifications={classifications} />
+      </Grid>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={4}>
+            <Table columns={sampleListCol} rows={tableData} checkbox={true} />
+          </Grid>
+          {/* <Grid item xs={6} md={8}>
+            <Table />
+          </Grid> */}
+        </Grid>
+      </Box>
+    </Fragment>
+  );
+}
